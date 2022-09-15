dfc1c7bde5502caaf2643bf44c24391ab380958e
Queite Schneider Castiglioni <queitesc@gmail.com>
2022-09-15 13:41:56 -0300

    :white_check_mark: test: end cars model tests

--- src/tests/mocks/carsMock.ts
+++ src/tests/mocks/carsMock.ts
@@ -25,3 +25,22 @@ export const createWithErrorCarMock = {
   doorsQty: 2,
   seatsQty: 2,
 }
+
+export const carMockForUpdateWithId = {
+  id: "4edd40c86762e0fb12000003",
+  model: "Fiat Uno",
+  year: 1963,
+  color: "blue",
+  buyValue: 3500,
+  seatsQty: 4,
+  doorsQty: 4
+}
+
+export const carMockForUpdate = {
+  model: "Fiat Uno",
+  year: 1963,
+  color: "blue",
+  buyValue: 3500,
+  seatsQty: 4,
+  doorsQty: 4
+}
--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -1,8 +1,9 @@
 import * as sinon from 'sinon';
 import chai from 'chai';
 import { Model } from 'mongoose';
-import { carWithIdMock, createCarMock } from '../../mocks/carsMock';
+import { carMockForUpdate, carMockForUpdateWithId, carWithIdMock, createCarMock } from '../../mocks/carsMock';
 import CarModel from '../../../models/Car';
+import { ErrorTypes } from '../../../errors/catalog';
 const { expect } = chai;
 
 describe('Model Cars', () => {
@@ -10,6 +11,10 @@ describe('Model Cars', () => {
 
   before(async () => {
     sinon.stub(Model, 'create').resolves(carWithIdMock);
+    sinon.stub(Model, 'find').resolves([carWithIdMock]);
+    sinon.stub(Model, 'findOne').resolves(carWithIdMock);
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(carMockForUpdateWithId);
+    sinon.stub(Model, 'findOneAndRemove').resolves(carWithIdMock);
   });
 
   after(()=>{
@@ -23,5 +28,54 @@ describe('Model Cars', () => {
     });
   })
 
+  describe('Get cars', () => {
+    it('should get all cars successfully', async () => {
+      const cars = await carModel.read();
+      expect(cars).to.be.deep.eq([carWithIdMock]);
+    });
+
+    it('should get cars by ID successfully', async () => {
+      const car = await carModel.readOne('632242cce100cb6a21015928');
+      expect(car).to.be.deep.eq(carWithIdMock);
+    });
+
+    it('should to throw the error "InvalidMongoId" when the ID is wrong', async () => {
+			try {
+				await carModel.readOne('123ERRADO');
+			} catch (error:any) {
+				expect(error.message).to.be.eq(ErrorTypes.InvalidMongoId);
+			}
+		});
+  })
+
+  describe('Update car', () => {
+    it('successfully', async () => {
+			const car = await carModel.update('4edd40c86762e0fb12000003', carMockForUpdate);
+			expect(car).to.be.deep.equal(carMockForUpdateWithId);
+		});
+
+		it('should to throw the error "InvalidMongoId" when the ID is wrong', async () => {
+			try {
+				await carModel.update('123ERRADO', carMockForUpdate);
+			} catch (error:any) {
+				expect(error.message).to.be.eq(ErrorTypes.InvalidMongoId);
+			}
+		});
+  })
+
+  describe('Delete car by ID', () => {
+    it('should delete a car successfully', async () => {
+      const deletedCar = await carModel.delete('632242cce100cb6a21015928');
+      expect(deletedCar).to.be.deep.eq(carWithIdMock);
+    });
+
+    it('should to throw the error "InvalidMongoId" when the ID is wrong', async () => {
+			try {
+				await carModel.delete('123ERRADO');
+			} catch (error:any) {
+				expect(error.message).to.be.eq(ErrorTypes.InvalidMongoId);
+			}
+		});
+  })
 
 });
